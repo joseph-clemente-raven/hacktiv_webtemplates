47139ecbd4dc94ad02f81583091d13859f1f251d
Joseph <joseph.clemente@hacktiv.io>
2023-10-02 18:56:36 +0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#5F8793',
+          secondary: '',
+          tertiary: ''
+        },
+        fontFamily: {},
+      },
+    },
+    plugins: [],
+};
